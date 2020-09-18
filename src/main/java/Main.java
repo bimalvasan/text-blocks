74674cfbf5e1e66aec1405a9b331cd3ad1b90c6d
bimalvasan <bimalvasan@gmail.com>
2020-09-18 08:39:48 -0500

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args){
+        var textBlock = """
+                <person>
+                    <firstName>Bimal</firstName>
+                    <lastName>Vasan</lastName>
+                    <age>35</age>
+                </person>
+                """.trim().toUpperCase();
+
+        System.out.println(textBlock);
+    }
+}
